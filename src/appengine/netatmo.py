e1cbfa68661f53c9595defe462a28ec1d7b0de9a
Tom Wilkie <tom.wilkie@gmail.com>
2014-01-25 14:49:59 +0000

    Credentials for netatmo account

---
+++ src/appengine/netatmo.py
@@ -0,0 +1,7 @@
+netatmo_cred = {
+        "client_id": 52daaaea187759c10c7b23fd,
+        "client_secret": lfUPaKasvdnao2lHxpTI7kBbUvjN67wnxKCNje,
+        "request_token_url": http://api.netatmo.net/oauth2/token
+        "authorize_url": http://api.netatmo.net/oauth2/authorize
+        "callback_url": None
+}
