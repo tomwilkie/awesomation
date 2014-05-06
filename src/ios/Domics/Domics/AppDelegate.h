6df7df514da07aff0a019ef2191c94c83d4f7ab9
Tom Wilkie <tom.wilkie@gmail.com>
2014-05-06 20:32:39 +0100

    iOS app; delete arduino code; update control.py for rcswitch

---
+++ src/ios/Domics/Domics/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Domics
+//
+//  Created by Tom Wilkie on 20/04/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- src/pi/control.py
+++ src/pi/control.py
@@ -2,44 +2,32 @@ import sys
 import urllib2
 import logging
 import json
-import serial
-import serial.tools.list_ports
 import time
 import traceback
 
 from common import creds
 from pusherclient import Pusher
 
-def find_arduino():
-    # These are the vendor and product ids for the 
-    # arduino's ftdi chip, I think
-    ports = list(serial.tools.list_ports.grep(r'VID:PID=2341:43'))
-    if not ports:
-        sys.stderr.write("No Arduino found.\n")
-        sys.exit(-1)
-    if len(ports) > 1:
-        sys.stderr.write("Too many Arduino's found.\n")
-        sys.exit(-1)
-    return ports[0][0]
+PIN=3
 
 def main():
     pusher = Pusher(creds.pusher_key)
-    ser = serial.Serial(find_arduino(), 9600)
-
-    def sendToArduino(message):
-        print message
-        ser.write(message)
+    switch = rcswitch.RCSwitch()
+    switch.enableTransmit(PIN)
 
     def callback(data):
         try:
             data = json.loads(data)
             print data
-            operation = 1 if data["mode"] else 0
+            operation = 1 
             system_code = data["system_code"]
-            device_code = data["device_code"]
-            message = "%s%s%s\n" % (operation, system_code, device_code)
-            message = message.encode('ascii','ignore')
-            sendToArduino(message)
+            device_code = int(data["device_code"])
+            
+            if data["mode"]:
+              switch.switchOn(system_code, device_code)
+            else:
+              switch.switchOff(system_code, device_code)
+            
         except Exception, e:
             print e
             traceback.print_exc()
@@ -54,6 +42,7 @@ def main():
 
     while True:
         time.sleep(100)
+
 
 if __name__ == '__main__':
     main()
