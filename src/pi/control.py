a89ac2ff3c25a8a32eeb65ca1afd182b02310b57
Tom Wilkie <tom.wilkie@gmail.com>
2014-11-21 18:02:01 +0000

    Beginning of zwave integration on pi

--- src/pi/control.py
+++ src/pi/control.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import argparse
 import json
 import logging
 import rcswitch
@@ -10,10 +11,11 @@ import urllib2
 
 from common import creds
 from pusherclient import Pusher
+import libopenzwave
 
 PIN=3
 
-def main():
+def Pusher():
     pusher = Pusher(creds.pusher_key)
     switch = rcswitch.RCSwitch()
     switch.enableTransmit(PIN)
@@ -45,6 +47,33 @@ def main():
 
     while True:
         time.sleep(100)
+
+
+def ZWaveMain(device):
+  config_path = libopenzwave.configPath()
+  user_path = '.'
+  cmd_line = ''
+  options = libopenzwave.PyOptions(config_path, user_path, cmd_line)
+  options.lock()
+  
+  def Callback(ars):
+    print args
+  
+  manager = libopenzwave.PyManager()
+  manager.create()
+  manager.addWatcher(Callback)
+  manager.addDriver(device)
+  
+  while True:
+    time.sleep(100)
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('zwave_device', default='/dev/ttyUSB0')
+  args = parser.parse_args()
+
+  ZWaveMain(args.zwave_device)
 
 
 if __name__ == '__main__':
