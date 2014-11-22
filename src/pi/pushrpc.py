11edf3d64adbc6417294201d31a3091dc49a99d8
Tom Wilkie <tom.wilkie@gmail.com>
2014-11-22 15:02:55 +0000

    Get z wave events printing out.

--- src/pi/pushrpc.py
+++ src/pi/pushrpc.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from common import creds
 from pusherclient import Pusher
--- src/pi/zwave.py
+++ src/pi/zwave.py
@@ -4,10 +4,16 @@ class ZWave(object):
   
   def __init__(self, device):
     self._device = device
-    manager = libopenzwave.PyManager()
-    manager.create()
-    manager.addWatcher(self._Callback)
-    manager.addDriver(self._device)
+    
+    self._options = libopenzwave.PyOptions()
+    self._options.create(
+      libopenzwave.configPath(), '.', '')
+    self._options.lock()
+    
+    self._manager = libopenzwave.PyManager()
+    self._manager.create()
+    self._manager.addWatcher(self._Callback)
+    self._manager.addDriver(self._device)
 
   def _Callback(self, args):
     print args
