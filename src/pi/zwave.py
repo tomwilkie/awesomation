b53267a08748b92cba77aed2345e1c87df83e68b
Tom Wilkie <tom.wilkie@gmail.com>
2014-11-23 10:36:02 +0000

    Try and listen to z wave device state changes.

--- src/pi/zwave.py
+++ src/pi/zwave.py
@@ -1,3 +1,5 @@
+import logging
+
 import libopenzwave
 
 class ZWave(object):
@@ -8,6 +10,7 @@ class ZWave(object):
     self._options = libopenzwave.PyOptions()
     self._options.create(
       libopenzwave.configPath(), '.', '')
+    self._options.addOptionBool("ConsoleOutput", False)
     self._options.lock()
     
     self._manager = libopenzwave.PyManager()
@@ -15,5 +18,24 @@ class ZWave(object):
     self._manager.addWatcher(self._Callback)
     self._manager.addDriver(self._device)
 
-  def _Callback(self, args):
-    print args
+  def _Callback(self, data):
+    notification_type = data['notificationType']
+    node_id = data['nodeId']
+    
+    if notification_type == 'Notification':
+      logging.debug('ZWave callback - %s', data)
+      return
+    
+    logging.info('ZWave callback - %s', data)
+    
+    if notification_type == 'NodeAdded':
+      if node_id == 1:
+        return
+      
+      self._manager.addAssociation(0, node_id, 1, 1)
+
+  def HandleEvent(self, event):
+    pass
+
+  def Stop(self):
+    self._manager.stop()
