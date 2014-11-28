02c846c2f79556789289ab17bc0a675a2dc8bb03
Tom Wilkie <tom.wilkie@gmail.com>
2014-11-28 16:05:55 +0000

    Hard code some of the open xwave config.

--- src/pi/zwave.py
+++ src/pi/zwave.py
@@ -6,6 +6,8 @@ import sys
 import libopenzwave
 
 
+CONFIG = '/usr/local/share/python-openzwave/config'
+
 class ZWave(object):
   """ZWave proxy object."""
 
@@ -14,15 +16,18 @@ class ZWave(object):
     self._callback = callback
 
     self._options = libopenzwave.PyOptions()
-    self._options.create(
-        libopenzwave.configPath(), '.', '')
-    self._options.addOptionBool("ConsoleOutput", False)
+    self._options.create(CONFIG, '/home/pi/openzwave', '')
+    self._options.addOptionBool('ConsoleOutput', False)
+    self._options.addOptionInt('SaveLogLevel', 6) # INFO
+    self._options.addOptionInt('QueueLogLevel', 6) # INFO
     self._options.lock()
 
     self._manager = libopenzwave.PyManager()
     self._manager.create()
     self._manager.addWatcher(self._zwave_callback)
     self._manager.addDriver(self._device)
+
+    self._home_id = None
 
   def _zwave_callback(self, data):
     # pylint: disable=broad-except
@@ -34,7 +39,7 @@ class ZWave(object):
   def _zwave_callback_internal(self, data):
     """Handle zwave events."""
     notification_type = data['notificationType']
-    home_id = data['homeId']
+    self._home_id = data['homeId']
     node_id = data['nodeId']
 
     logging.info('ZWave callback - %d %s', node_id, notification_type)
@@ -43,18 +48,23 @@ class ZWave(object):
       return
 
     if notification_type == 'NodeAdded':
-      self._manager.addAssociation(home_id, node_id, 1, 1)
-      self._manager.refreshNodeInfo(home_id, node_id)
-      self._manager.requestNodeState(home_id, node_id)
-      self._manager.requestNodeDynamic(home_id, node_id)
+      self._manager.addAssociation(self._home_id, node_id, 1, 1)
+      self._manager.refreshNodeInfo(self._home_id, node_id)
+      self._manager.requestNodeState(self._home_id, node_id)
+      self._manager.requestNodeDynamic(self._home_id, node_id)
 
     elif notification_type in {'ValueAdded', 'ValueChanged', 'NodeNaming'}:
       #logging.info('%s', data)
       self._callback('zwave', 'zwave-%d' % node_id, data)
 
+    else: #if notification_type in {'NodeEvent'}:
+      logging.info(data)
+
   def handle_event(self, event):
     pass
 
   def stop(self):
+    if self._home_id is not None:
+      self._manager.writeConfig(self._home_id)
     self._manager.removeWatcher(self._callback)
     self._manager.removeDriver(self._device)
