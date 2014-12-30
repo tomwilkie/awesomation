7b3842852076e60cf46a5f5d2a11a456f3e5f4ab
Tom Wilkie <tom.wilkie@gmail.com>
2014-12-30 12:10:41 +0000

    This code is no longer needed.

--- src/appengine/room.py
+++ src/appengine/room.py
@@ -14,18 +14,6 @@ class Room(model.Base):
   name = ndb.StringProperty(required=False)
   devices = ndb.KeyProperty(repeated=True)
 
-  def handle_command(self, command_dict):
-    """Dispatch command to appropriate handler."""
-    logging.info(command_dict)
-    func_name = command_dict.pop('command', None)
-    func = getattr(self, func_name, None)
-    logging.info('%s %s %s', func, type(func), func.is_command)
-    if func is None or not func.is_command:
-      logging.error('Command %s does not exist or is not a command',
-                    func_name)
-      flask.abort(400)
-    func(**command_dict)
-
   @rest.command
   def all_on(self):
     switches = device.Switch.query().filter(
