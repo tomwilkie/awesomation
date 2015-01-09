86c5d600ee4f289bba275f385fdd822e471d028b
Tom Wilkie <tom.wilkie@gmail.com>
2015-01-09 14:27:13 +0000

    Refactor some of control.py so it can start without wave etc.

--- src/pi/control.py
+++ src/pi/control.py
@@ -15,19 +15,41 @@ class Control(object):
   """'Controller class, ties proxies and rpc together."""
 
   def __init__(self, args):
+    self._pusher = pushrpc.PushRPC(self._push_event_callback)
+
+    # These modules should work 100% of the time -
+    # they don't need special hardware, or root
     self._proxies = {
-        'rfswitch': rfswitch.RFSwitch(args.rfswtich_pin),
-        'zwave': zwave.ZWave(args.zwave_device, self._device_event_callback),
         'hue': hue.Hue(args.hue_scan_interval_secs,
                        self._device_event_callback),
         'wemo': wemo.Wemo(args.hue_scan_interval_secs,
                           self._device_event_callback),
-        'network': network.NetworkMonitor(
-            self._device_event_callback, args.network_scan_interval_secs,
-            args.network_scan_timeout_secs),
     }
 
-    self._pusher = pushrpc.PushRPC(self._push_event_callback)
+    # This module needs root, so might not work
+    try:
+      self._proxies['network'] = network.NetworkMonitor(
+          self._device_event_callback,
+          args.network_scan_interval_secs,
+          args.network_scan_timeout_secs)
+    except:
+      logging.error('Failed to initialize network module',
+                    exc_info=sys.exc_info())
+
+    # This module needs root, so might not work
+    try:
+      self._proxies['rfswitch'] = rfswitch.RFSwitch(args.rfswtich_pin)
+    except:
+      logging.error('Failed to initialize rfswitch module',
+                    exc_info=sys.exc_info())
+
+    # This module needs a zwave usb stick
+    try:
+      self._proxies['zwave'] = zwave.ZWave(
+          args.zwave_device, self._device_event_callback)
+    except:
+      logging.error('Failed to initialize zwave module',
+                    exc_info=sys.exc_info())
 
   def stop(self):
     self._pusher.stop()
@@ -65,11 +87,16 @@ def main():
   logging.getLogger().addHandler(file_handler)
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('--zwave_device', default='/dev/ttyUSB0')
-  parser.add_argument('--rfswtich_pin', default=3)
-  parser.add_argument('--hue_scan_interval_secs', default=5*60)
-  parser.add_argument('--network_scan_interval_secs', default=10)
-  parser.add_argument('--network_scan_timeout_secs', default=5*60)
+  parser.add_argument('--zwave_device',
+                      default='/dev/ttyUSB0')
+  parser.add_argument('--rfswtich_pin',
+                      default=3)
+  parser.add_argument('--hue_scan_interval_secs',
+                      default=5*60)
+  parser.add_argument('--network_scan_interval_secs',
+                      default=10)
+  parser.add_argument('--network_scan_timeout_secs',
+                      default=5*60)
   args = parser.parse_args()
 
   control = Control(args)
