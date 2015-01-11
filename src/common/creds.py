6d37c82b2e63c5f383673284ce55407809e6bdce
Tom Wilkie <tom.wilkie@gmail.com>
2015-01-11 18:44:13 +0000

    Remove reds subrepo and checkin a dummy private reds file.

---
+++ src/common/creds.py
@@ -0,0 +1,13 @@
+"""Private credentials."""
+
+pusher_app_id = None
+pusher_secret = None
+
+NEST_CLIENT_ID = None
+NEST_CLIENT_SECRET = None
+
+NETATMO_CLIENT_ID = None
+NETATMO_CLIENT_SECRET = None
+
+AWS_ACCESS_KEY_ID = None
+AWS_SECRET_ACCESS_KEY = None
