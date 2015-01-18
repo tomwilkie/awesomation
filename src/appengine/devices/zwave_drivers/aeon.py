8c51275884848654f5da94f14559f21edb2b7fa1
Tom Wilkie <tom.wilkie@gmail.com>
2015-01-18 16:04:28 +0000

    Get zwave motion sensors working a little better.

--- src/appengine/devices/zwave_drivers/aeon.py
+++ src/appengine/devices/zwave_drivers/aeon.py
@@ -4,4 +4,21 @@ from appengine.devices import zwave
 
 @zwave.register(manufacturer_id='0086', product_type='0002', product_id='0005')
 class AeonLabsMultiSensor(zwave.Driver):
-  pass
+  """Driver for Aeon Labs Multi Sensor."""
+
+  def _set(self, command_class, index, intended_value):
+    ccv = self._device.get_command_class_value(command_class, index)
+    if ccv.value != intended_value:
+      ccv.set_value(intended_value)
+
+  def configure_device(self):
+    """Configure this device to send value changes (instead of basic set),
+       on motion, and temp, humidity and luminance every 5 mins."""
+    self._set('COMMAND_CLASS_CONFIGURATION', 4, 'Binary Sensor Report')
+    self._set('COMMAND_CLASS_CONFIGURATION', 101, 0b11100001)
+    self._set('COMMAND_CLASS_CONFIGURATION', 111, 5*60)
+
+  def handle_event(self, event):
+    if event['notificationType'] in {zwave.NODE_ADDED, zwave.NODE_INFO_UPDATE}:
+      self.configure_device()
+
