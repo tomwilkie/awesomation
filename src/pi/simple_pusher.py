c715cf3a892882a8a3e3e527e4b61cac876fa83b
Tom Wilkie <tom.wilkie@gmail.com>
2015-01-25 12:12:08 +0000

    Refactor pusher server to remove some globals.

--- src/pi/simple_pusher.py
+++ src/pi/simple_pusher.py
@@ -1,4 +1,5 @@
 """A really dump pusher 'clone', for use in testing and running locally."""
+import argparse
 import collections
 import json
 import logging
@@ -12,15 +13,15 @@ import SimpleWebSocketServer
 
 HTTP_PORT = 8101
 WEBSOCKET_PORT = 8102
-SOCKETS = collections.defaultdict(list)
+LOGFMT = '%(asctime)s %(levelname)s %(filename)s:%(lineno)d - %(message)s'
 
 
 class SocketHandler(SimpleWebSocketServer.WebSocket):
   """Represents a websocket connection."""
   # pylint: disable=invalid-name
-
-  def __init__(self, *args, **kwargs):
-    super(SocketHandler, self).__init__(*args, **kwargs)
+  def __init__(self, sockets, server, sock, address):
+    super(SocketHandler, self).__init__(server, sock, address)
+    self._sockets = sockets
     self._channels = []
 
   def handleMessage(self):
@@ -33,7 +34,7 @@ class SocketHandler(SimpleWebSocketServer.WebSocket):
       logging.info('\'%s\' received', self.data)
       data = json.loads(self.data.decode('utf-8'))
       self._channels.append(data['channel'])
-      SOCKETS[data['channel']].append(self)
+      self._sockets[data['channel']].append(self)
     except:
       logging.error('Error handling message:', exc_info=sys.exc_info())
 
@@ -43,16 +44,19 @@ class SocketHandler(SimpleWebSocketServer.WebSocket):
   def handleClose(self):
     logging.info('%s closed', self.address, exc_info=sys.exc_info())
     for channel in self._channels:
-      SOCKETS[channel].remove(self)
+      self._sockets[channel].remove(self)
 
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
   """Represents a http requests."""
   # pylint: disable=invalid-name,too-many-public-methods
+  def __init__(self, sockets, request, client_address, server):
+    self._sockets = sockets
+    SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(
+        self, request, client_address, server)
 
   def do_POST(self):
     """Send request body to /channel."""
-
     try:
       channel = self.path.split('/')[-1]
       content_len = int(self.headers.getheader('content-length', 0))
@@ -60,7 +64,7 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
       #logging.info('Sending \"%s\" to \"%s\"', post_body, channel)
 
-      for socket in SOCKETS[channel]:
+      for socket in self._sockets[channel]:
         socket.sendMessage(post_body)
 
       self.send_response(204, '')
@@ -70,22 +74,31 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
 class SimplePusher(object):
   """A very simple websocket / push service."""
-  def __init__(self):
+  def __init__(self, args):
+    self._args = args
+    self._sockets = collections.defaultdict(list)
     self._httpd = None
     self._httpd_thread = None
     self._websocket_server = None
     self._websocket_server_thread = None
 
+  def _http_request_handler(self, request, client_address, server):
+    return ServerHandler(self._sockets, request, client_address, server)
+
+  def _websocket_request_handler(self, server, sock, addr):
+    return SocketHandler(self._sockets, server, sock, addr)
+
   def start(self):
     """Start this."""
     logging.info('Starting local websocket server.')
 
-    self._httpd = SocketServer.TCPServer(('', HTTP_PORT), ServerHandler)
+    self._httpd = SocketServer.TCPServer(
+        ('', self._args.http_port), self._http_request_handler)
     self._httpd_thread = threading.Thread(target=self._httpd.serve_forever)
     self._httpd_thread.start()
 
     self._websocket_server = SimpleWebSocketServer.SimpleWebSocketServer(
-          '', WEBSOCKET_PORT, SocketHandler)
+        '', self._args.websocket_port, self._websocket_request_handler)
     self._websocket_server_thread = threading.Thread(
         target=self._websocket_server.serveforever)
     self._websocket_server_thread.start()
@@ -99,10 +112,18 @@ class SimplePusher(object):
     self._websocket_server_thread.join()
 
 
-if __name__ == '__main__':
-  logging.basicConfig(level=logging.INFO)
-  PUSHER = SimplePusher()
-  PUSHER.start()
+def main():
+  logging.basicConfig(format=LOGFMT, level=logging.DEBUG)
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--http_port',
+                      default=HTTP_PORT)
+  parser.add_argument('--websocket_port',
+                      default=WEBSOCKET_PORT)
+  args = parser.parse_args()
+
+  pusher = SimplePusher(args)
+  pusher.start()
 
   try:
     while True:
@@ -110,5 +131,9 @@ if __name__ == '__main__':
   except:
     pass
 
-  PUSHER.stop()
+  pusher.stop()
+
+
+if __name__ == '__main__':
+  main()
 
